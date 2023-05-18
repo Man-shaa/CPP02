9ebc4f137355c7391753712c6df1b77cc11407a4
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-05-18 18:02:00 +0200

    [UP] main.cpp ex02 + [FIX] static min/max pour utiliser sans objet de type Fixed

--- ex02/fixed.hpp
+++ ex02/fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 20:53:29 by msharifi          #+#    #+#             */
-/*   Updated: 2023/04/29 14:47:08 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/05/18 18:00:23 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,25 +29,25 @@ class	Fixed
 		Fixed(const Fixed &toCopy);
 		~Fixed(void);
 
-		Fixed&	operator = (const Fixed &toCopy);
-		Fixed	operator + (const Fixed &fixed) const;
-		Fixed	operator - (const Fixed &fixed) const;
-		Fixed	operator / (const Fixed &fixed) const;
-		Fixed	operator * (const Fixed &fixed) const;
-		bool	operator < (const Fixed &fixed) const;
-		bool	operator <= (const Fixed &fixed) const;
-		bool	operator > (const Fixed &fixed) const;
-		bool	operator >= (const Fixed &fixed) const;
-		bool	operator == (const Fixed &fixed) const;
-		bool	operator != (const Fixed &fixed) const;
-		Fixed	operator ++ (int);
-		Fixed	&operator ++ (void);
-		Fixed	operator -- (int);
-		Fixed	&operator -- (void);
-		Fixed	min(Fixed const &a, Fixed const &b);
-		Fixed	min(Fixed &a, Fixed &b);
-		Fixed	max(Fixed const &a, Fixed const &b);
-		Fixed	max(Fixed &a, Fixed &b);
+		Fixed&			operator = (const Fixed &toCopy);
+		Fixed			operator + (const Fixed &fixed) const;
+		Fixed			operator - (const Fixed &fixed) const;
+		Fixed			operator / (const Fixed &fixed) const;
+		Fixed			operator * (const Fixed &fixed) const;
+		bool			operator < (const Fixed &fixed) const;
+		bool			operator <= (const Fixed &fixed) const;
+		bool			operator > (const Fixed &fixed) const;
+		bool			operator >= (const Fixed &fixed) const;
+		bool			operator == (const Fixed &fixed) const;
+		bool			operator != (const Fixed &fixed) const;
+		Fixed			operator ++ (int);
+		Fixed			&operator ++ (void);
+		Fixed			operator -- (int);
+		Fixed			&operator -- (void);
+		static Fixed	min(Fixed const &a, Fixed const &b);
+		static Fixed	min(Fixed &a, Fixed &b);
+		static Fixed	max(Fixed const &a, Fixed const &b);
+		static Fixed	max(Fixed &a, Fixed &b);
 		
 
 		float	toFloat(void) const;
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,24 +6,41 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 16:05:46 by msharifi          #+#    #+#             */
-/*   Updated: 2023/04/28 21:00:19 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/05/18 18:01:23 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fixed.hpp"
 int main(void)
 {
-	Fixed a(40);
+	// int	n = 0;
+	// if (n++ == 1)
+	// 	std::cout << "n++ = 1" << std::endl;
+	// n = 0;
+	// if (++n == 1)
+	// 	std::cout << "++n = 1" << std::endl;
 
+	Fixed a(40);
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	// std::cout << a << std::endl;
-	// std::cout << ++a << std::endl;
-	// std::cout << a << std::endl;
-	// std::cout << a++ << std::endl;
-	// std::cout << a << std::endl;
-	// std::cout << b << std::endl;
-	// std::cout << Fixed::max( a, b ) << std::endl;
+
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::min( a, b ) << std::endl;
+
+	Fixed	c(a);
+
+	std::cout << c << std::endl;
+
+	Fixed	d;
+	d = a;
+
+	std::cout << d << std::endl;
+
 	return (0);
 }
