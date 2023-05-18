0530a6e3265841877b678429b76cfe034704ca4b
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-05-18 17:36:40 +0200

    [UP] main.cpp ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 13:58:06 by msharifi          #+#    #+#             */
-/*   Updated: 2023/04/28 17:04:48 by msharifi         ###   ########.fr       */
+/*   Updated: 2023/05/18 17:36:21 by msharifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,10 @@ int	main(void)
 	std::cout << "b : " << b.getRawBits() << std::endl;
 	std::cout << "c : " << c.getRawBits() << std::endl;
 
+	// Fixed d(a);
+
+	// std::cout << "d : " << d.getRawBits() << std::endl;
+
 	b.setRawBits(-12);
 
 	std::cout << std::endl << "After b.setRawBits(-12)" << std::endl << std::endl;
@@ -51,6 +55,14 @@ int	main(void)
 	c.setRawBits(35);
 
 	std::cout << std::endl << "After c.setRawBits(35)" << std::endl << std::endl;
+
+	std::cout << "a : " << a.getRawBits() << std::endl;
+	std::cout << "b : " << b.getRawBits() << std::endl;
+	std::cout << "c : " << c.getRawBits() << std::endl;
+
+	c = b;
+
+	std::cout << std::endl << "After c = b" << std::endl << std::endl;
 
 	std::cout << "a : " << a.getRawBits() << std::endl;
 	std::cout << "b : " << b.getRawBits() << std::endl;
